be29bc029cd9e64574222010d2399500f9e10746
Jealous Matsikachando <pasej5@gmail.com>
2023-03-20 03:43:28 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Software is eating the World
+ * description: all possible combinations
+ * Return: always 0 on success
+ */
+
+int main(void)
+{
+	int j;
+	int k;
+
+		for (j = 0 ; j <= 98 ; j++)
+		{
+			for (k = 0 ; k <= 98 ; k++)
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			putchar(' ');
+			putchar((k / 10) + '0');
+			putchar((k % 10) + '0');
+				if (j == 98 && k == 99)
+					continue;
+				putchar(',');
+			putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
