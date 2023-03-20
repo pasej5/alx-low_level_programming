9794a45f9539be347636b398b24896b980a5e56b
Jealous Matsikachando <pasej5@gmail.com>
2023-03-20 01:11:47 -0700

    The success combination in business is

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,22 +13,24 @@ int main(void)
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		for (j = 1 ; j < 10 ; j++)
-			for (k = 2 ; k < 2; k++)
+	for (j = 1 ; j < 10 ; j++)
+		{
+		for (k = 2 ; k < 10; k++)
 			{
-				if (i < j && j < k)
-				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(k + '0');
-					if (i + j + k != 24)
+			if (i < j && j < k)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (i + j + k != 24)
 					{
-						putchar(',');
-						putchar(' ');
+					putchar(',');
+					putchar(' ');
+					}
 					}
 				}
 			}
 	}
-	putchar('\n');
-	return (0);
+		putchar('\n');
+		return (0);
 }
