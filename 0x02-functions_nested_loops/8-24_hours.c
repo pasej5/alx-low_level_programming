c37c4d5bdbd6fa8a85ee74684ce590c0add6373e
Jealous Matsikachando <pasej5@gmail.com>
2023-03-21 16:44:47 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,12 +3,26 @@
 /**
  * jack_bauer - function that prints every minute of the day of Jack Bauer
  *
- * @void - starting from 00:00 to 23:59
+ *
  *
  * Return: always
- * */
+ */
 
 void jack_bauer(void)
 {
+	int hr;
+	int min;
 
+	for (hr = 0 ; hr < 24 ; hr++)
+	{
+		for (min = 0 ; min < 60 ; min++)
+		{
+			_putchar(hr / 10 + '0');
+			_putchar(hr % 10 + '0');
+			_putchar(':');
+			_putchar(min / 10 + '0');
+			_putchar(min % 10 + '0');
+			_putchar('\n');
+		}
+	}
 }
