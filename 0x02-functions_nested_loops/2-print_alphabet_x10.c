b578d775dc489d26188c6d093a7cc181ecaf9744
Jealous Matsikachando <pasej5@gmail.com>
2023-03-21 03:41:35 -0700

    function that prints 10 times the alphabet, in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -15,9 +15,9 @@ void print_alphabet_x10(void)
 	for (i = 1 ; i <= 10 ; i++)
 	{
 		for (j = 'a' ; j <= 'z' ; j++)
-			{
-				_putchar(j);
-			}
-		_putchar('\n');
+		{
+			_putchar(j);
+		}
+	_putchar('\n');
 	}
 }
