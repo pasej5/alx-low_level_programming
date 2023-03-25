4175190a324ad3b6f25f4005fb1182fdd64a4cf6
Jealous Matsikachando <pasej5@gmail.com>
2023-03-24 17:17:08 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_numbers - check the code
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 
 void print_numbers(void)
@@ -12,6 +12,8 @@ void print_numbers(void)
 	char x;
 
 	for (x = 0; x <=9; x++)
+	{
 	_putchar(x + '0');
+	}
 	_putchar('\n');
 }
