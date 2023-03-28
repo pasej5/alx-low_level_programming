f4d9abed4ad80fea0e2bdb3377c9051b7d0cbbaa
Jealous Matsikachando <pasej5@gmail.com>
2023-03-27 17:20:47 -0700

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,11 +9,16 @@
 void puts2(char *str)
 {
 	int i;
+	int j = 0;
 
-	for (i = 0 ; str[i] != '\n' ; i++)
+	while (str[j] != '\0')
 	{
-		_putchar(str[i]);
-		i++;
+	j++;
+	}
+
+	for (i = 0; i < j; i += 2)
+	{
+	_putchar(str[i]);
 	}
 	_putchar('\n');
 }
