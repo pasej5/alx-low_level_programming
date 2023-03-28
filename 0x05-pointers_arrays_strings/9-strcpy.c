2082c81ae56d01d0e2227099b4eeb67c52ee35bf
Jealous Matsikachando <pasej5@gmail.com>
2023-03-27 18:53:54 -0700

    First, solve the problem. Then, write the code

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,13 +10,11 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i;
-
-	if (dest == NULL)
-	{
-	return (NULL);
-	}
-	for (i = 0 ; src[i] != '\0'; i++)
-		dest[i] = src[i];
-	return (dest);
+int i;
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+dest[i++] = '\0';
+return (dest);
 }
