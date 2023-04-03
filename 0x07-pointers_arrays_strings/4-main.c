c06bd9f559a6918b90ee13858095981c6e55ef01
Jealous Matsikachando <pasej5@gmail.com>
2023-04-03 14:34:46 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-main.c
+++ 0x07-pointers_arrays_strings/4-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - check the code
@@ -14,7 +15,7 @@ int main(void)
 	char *f = "world";
 	char *t;
 
-	t = _strpbrk(s, f);
+	t = strpbrk(s, f);
 	printf("%s\n", t);
 	return (0);
 }
