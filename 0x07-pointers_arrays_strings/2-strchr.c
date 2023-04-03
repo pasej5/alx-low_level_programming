c553e126f6db6b616ed8f69dad5f80082e795552
Jealous Matsikachando <pasej5@gmail.com>
2023-04-03 13:46:27 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,16 +7,22 @@
  * Return: Returns a pointer to the first occurrence of c
  */
 
-char *_strchr(char *s, char c)
+unsigned int _strspn(char *s, char *accept)
 {
-	int m = 0;
+	unsigned int c = 0;
+	char *t = accept;
 
-	while (s[m] >= '0')
-
+	while (*s++)
 	{
-		if (s[m] == c)
-		m++;
-		return (&s[m]);
+		while (*accept++)
+			if (*(s - 1) == *(accept - 1))
+			{
+				c++;
+				break;
+			}
+		if (!(*--accept))
+			break;
+		accept = t;
 	}
-	return (0);
-}
+	return (c);
+} 
