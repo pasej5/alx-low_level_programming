e00dbd8b65f3b88039bc829c277b189b561aedce
Jealous Matsikachando <pasej5@gmail.com>
2023-04-05 17:17:46 -0700

    function that prints a string,

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,12 +10,11 @@
 
 void _puts_recursion(char *s)
 {
-{
-	if (s[0] != '\0')
+	if (*s == '\0')
 	{
-		putchar(s[0]);
-		_puts_recursion((s + 1));
-	}
-	else
 		putchar('\n');
+		return;
+	}
+	putchar(*s);
+	_puts_recursion(s + 1);
 }
