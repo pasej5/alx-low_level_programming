24abafac57ac7f9091167037a52ddb0032915bba
Jealous Matsikachando <pasej5@gmail.com>
2023-04-17 12:41:17 -0700

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - dog object
+ * @name: name of the dog
+ * @age: dog age
+ * @owner: the owner of the dog
+ * Return: 0 always
+ */
 
 struct dog
 {
