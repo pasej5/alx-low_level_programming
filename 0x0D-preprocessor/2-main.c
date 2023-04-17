ad5455d99f1ea64500a9e28fd4cc4e89fa5b876e
Jealous Matsikachando <pasej5@gmail.com>
2023-04-17 06:00:48 -0700

    program that prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - void function
+ *
+ * Return: always 0 on success
+ */
+
+int main(void)
+{
+	printf("we compiled this program using this file : %s\n", __FILE__);
+	return (0);
+}
