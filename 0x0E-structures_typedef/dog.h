20fc4b70fd27247313698e2a8bf3f8be6af6bc8c
Jealous Matsikachando <pasej5@gmail.com>
2023-04-17 19:34:49 -0700

    defining a new typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,6 +21,14 @@ struct dog
  */
 
 typedef struct dog dog_t;
+
+
+typedef struct dog
+{
+    char name[50];
+    int age;
+    char breed[50];
+}
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
