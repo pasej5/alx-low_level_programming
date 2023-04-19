31ce5da12bd28a7c9a212f5493650ec910aa19f7
Jealous Matsikachando <pasej5@gmail.com>
2023-04-19 13:35:28 -0700

    this function searches for an integer

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,6 +4,8 @@
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+int is_strictly_positive(int elem);
+int abs_is_98(int elem);
 void print_name_uppercase(char *name);
 
 #endif
