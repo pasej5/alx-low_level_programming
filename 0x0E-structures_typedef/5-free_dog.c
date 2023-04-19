ff8759dc5f835e5b9d188738814a7e617470e8c8
Jealous Matsikachando <pasej5@gmail.com>
2023-04-18 18:03:21 -0700

    a function that frees the dog function

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees the dog memory
+ * @d: structures to be freed
+ *
+ */
+
+void free_dog(dog_t *d) 
+{
+    free(d);
+}
