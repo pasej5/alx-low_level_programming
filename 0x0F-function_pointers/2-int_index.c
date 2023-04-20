45499de37cc572c4f46195ff2caa1ad509f24330
Jealous Matsikachando <pasej5@gmail.com>
2023-04-19 17:11:46 -0700

    function that searches for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,8 +13,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (size <= 0)
-	return (-1);
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
 
 	while (i < size)
 	{
