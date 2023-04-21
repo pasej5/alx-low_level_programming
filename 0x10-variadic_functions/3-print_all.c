d2eb29520a6c0b41a72b9f6c669330f6a163f2aa
Jealous Matsikachando <pasej5@gmail.com>
2023-04-20 17:59:28 -0700

    function that prints anything.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,50 +1,60 @@
-#include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
 
 /**
- * print_all - prints anything
+ * print_all - prints a list of variables with different data types
  * @format: list of types of arguments passed to the function
+ * c: char
+ * i: integer
+ * f: float
+ * s: char * (if the string is NULL, print (nil) instead)
+ * any other char should be ignored
+ * see example
+ *
+ * Return: void
  */
+
 void print_all(const char * const format, ...)
 {
-	int i = 0;
-	char *str, *sep = "";
+	va_list args;
+	int i = 0, j = 0;
+	char c, *s;
+	float f;
 
-	va_list list;
+	va_start(args, format);
 
-	va_start(list, format);
-
-	if (format)
+	while (format && format[i])
 	{
-		while (format[i])
+		switch (format[i])
 		{
-			switch (format[i])
-			{
-				case 'c':
-					printf("%s%c", sep, va_arg(list, int));
-					break;
-				case 'i':
-					printf("%s%d", sep, va_arg(list, int));
-					break;
-				case 'f':
-					printf("%s%f", sep, va_arg(list, double));
-					break;
-				case 's':
-					str = va_arg(list, char *);
-					if (!str)
-						str = "(nil)";
-					printf("%s%s", sep, str);
-					break;
-				default:
-					i++;
-					continue;
-			}
-			sep = ", ";
-			i++;
+			case 'c':
+				c = (char) va_arg(args, int);
+				printf("%c", c);
+				break;
+			case 'i':
+				j = va_arg(args, int);
+				printf("%d", j);
+				break;
+			case 'f':
+				f = (float) va_arg(args, double);
+				printf("%f", f);
+				break;
+			case 's':
+				s = va_arg(args, char *);
+				if (s == NULL)
+					printf("(nil)");
+				else
+					printf("%s", s);
+				break;
+			default:
+				i++;
+				continue;
 		}
+		if (format[i + 1] != '\0' && (format[i] == 'c' || format[i] == 'i' ||
+					      format[i] == 'f' || format[i] == 's'))
+			printf(", ");
+		i++;
 	}
-
 	printf("\n");
-	va
-
+	va_end(args);
+}
