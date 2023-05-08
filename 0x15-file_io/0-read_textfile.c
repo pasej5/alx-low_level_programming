cb7c3f2b1a39491209470d8a6e41ad8e76614c55
Jealous Matsikachando <pasej5@gmail.com>
2023-05-08 13:22:34 -0700

    function that reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,52 +5,52 @@
 
 /**
  * read_textfile - reads a text file and prints it to POSIX standard output
- * @filename: name of the file to read
- * @letters: number of letters to read and print
+ * @filename: pointer to name of the file to read
+ * @letters: actual number of letters it could read and print
  *
  * Return: actual number of letters read and printed, or 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t n_read, n_written;
+	int file_descriptor;
+	ssize_t byte_numbers_to_be_read, byte_numbers_to_be_printed;
 	char *buf;
 
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
 		return (0);
 
 	buf = malloc(letters + 1);
 	if (buf == NULL)
 	{
-		close(fd);
+		close(file_descriptor);
 		return (0);
 	}
 
-	n_read = read(fd, buf, letters);
-	if (n_read == -1)
+	byte_numbers_to_be_read = read(file_descriptor, buf, letters);
+	if (byte_numbers_to_be_read == -1)
 	{
-		close(fd);
+		close(file_descriptor);
+		free(buf);
+		return (0);
+	}
+	buf[byte_numbers_to_be_read] = '\0';
+	byte_numbers_to_be_printed =
+	write(STDOUT_FILENO, buf, byte_numbers_to_be_read);
+	if (byte_numbers_to_be_printed == -1 || byte_numbers_to_be_printed !=
+	byte_numbers_to_be_read)
+	{
+		close(file_descriptor);
 		free(buf);
 		return (0);
 	}
 
-	buf[n_read] = '\0';
-
-	n_written = write(STDOUT_FILENO, buf, n_read);
-	if (n_written == -1 || n_written != n_read)
-	{
-		close(fd);
-		free(buf);
-		return (0);
-	}
-
-	close(fd);
+	close(file_descriptor);
 	free(buf);
 
-	return (n_written);
+	return (byte_numbers_to_be_printed);
 }
 
