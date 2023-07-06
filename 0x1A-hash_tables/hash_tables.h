f3676447d5c69a7814ab57253e9d0f3b42b4c9c2
pasej5 <pasej5@gmail.com>
2023-07-06 10:26:32 +0000

    Creating an empty hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -2,5 +2,22 @@
 #define HASH_TABLES_H
 
 
+#include <stdlib.h>
 
-#endif 
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+}hash_node_t;
+
+
+typedef struct hash_tables_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+}hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
+
+#endif /* HASH_TABLES_H */
