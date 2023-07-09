b6f9862ae050895742930a8a94777ea268b8ffc3
pasej5 <pasej5@gmail.com>
2023-07-09 07:25:57 +0000

    fixing betty indentation

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node, *temp;
 
 	if (ht == NULL || key == NULL || *key == '\0')
-		return 0;
+		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 
@@ -28,32 +28,32 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			free(temp->value);
 			temp->value = strdup(value);
 			if (temp->value == NULL)
-				return 0;
-			return 1;
+				return (0);
+			return (1);
 		}
 		temp = temp->next;
 	}
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
-		return 0;
+		return (0);
 
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
 		free(new_node);
-		return 0;
+		return (0);
 	}
 	new_node->value = strdup(value);
 	if (new_node->value == NULL)
 	{
 		free(new_node->key);
 		free(new_node);
-		return 0;
+		return (0);
 	}
 
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
-	return 1;
+	return (1);
 }
