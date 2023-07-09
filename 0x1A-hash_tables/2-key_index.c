aa73c3a83a5cab2f4e005c528097be244f9018aa
pasej5 <pasej5@gmail.com>
2023-07-09 06:27:32 +0000

    betty style fix

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -13,10 +13,8 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 	unsigned long int hash_value;
 	unsigned long int index;
 
-	/* Calculate the hash value using the hash_djb2 function */
 	hash_value = hash_djb2(key);
 
-	/* Compute the index by taking the modulus of the hash value with the size of the array */
 	index = hash_value % size;
 
 	return (index);
