5db4fc6f779e0f25147ae7073437b7390c0f236a
Jealous Matsikachando <pasej5@gmail.com>
2023-10-12 03:43:19 -0700

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -2,19 +2,11 @@
 #include <math.h>
 #include "search_algos.h"
 
-/**
- * jump_search - Search for a value in a sorted array using Jump Search
- * @array: Pointer to the first element of the sorted array
- * @size: Number of elements in the array
- * @value: Value to search for
- *
- * Return: Index where 'value' is located, or -1 if not found
- */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t step = sqrt(size);
+	size_t step = (size_t)sqrt((double)size); /* Cast to double to avoid ISO C90 warning */
 	size_t left = 0;
-	size_t right = 0;
+	size_t right = step;
 	size_t i;
 
 	if (array == NULL)
@@ -25,27 +17,18 @@ int jump_search(int *array, size_t size, int value)
 		left = right;
 		right += step;
 
-		printf("Value checked array[%lu] = [%d]\n", right, array[right]);
-
-		if (right < size)
-		{
-			printf("Value checked array[%lu] = [%d]\n", right, array[right]);
-		}
-
-		if (array[right] >= value) // Check if the value is within the current block
-		{
-			break;
-		}
+		printf("Value checked array[%u] = [%d]\n", (unsigned int)right, array[right]); /* Cast to unsigned int to avoid ISO C90 warning */
 	}
 
-	printf("Value found between indexes [%lu] and [%lu]\n", left, right);
+	printf("Value found between indexes [%u] and [%u]\n", (unsigned int)left, (unsigned int)right); /* Cast to unsigned int to avoid ISO C90 warning */
 
 	for (i = left; i <= right && i < size; i++)
 	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		printf("Value checked array[%u] = [%d]\n", (unsigned int)i, array[i]); /* Cast to unsigned int to avoid ISO C90 warning */
 		if (array[i] == value)
 			return (i);
 	}
 
 	return (-1);
 }
+
