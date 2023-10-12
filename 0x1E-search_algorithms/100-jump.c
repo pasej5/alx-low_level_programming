352255a2cfd45bda71d975330ec16885de86f4da
Jealous Matsikachando <pasej5@gmail.com>
2023-10-12 02:19:59 -0700

    check whether value is still in range

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -31,6 +31,10 @@ int jump_search(int *array, size_t size, int value)
 			printf("Value checked array[%lu] = [%d]\n", right, array[right]);
 	}
 
+	if (array[right] >= value)
+	{
+		break;
+	}
 	printf("Value found between indexes [%lu] and [%lu]\n", left, right);
 
 	for (i = left; i <= right && i < size; i++)
