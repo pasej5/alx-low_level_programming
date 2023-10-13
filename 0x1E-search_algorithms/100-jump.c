3a9161b958f2c5f3f597c1ab0d07ff8c476ca3ed
Jealous Matsikachando <pasej5@gmail.com>
2023-10-12 18:47:09 -0700

    betty

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -18,10 +18,7 @@ int jump_search(int *array, size_t size, int value)
 	size_t right = 0;
 	size_t jump = sqrt(size);
 
-	if (jump <= 0)
-	{
-		jump = 1;
-	}
+	jump = jump > 0 ? jump : 1;
 	if (array == NULL || size == 0)
 		return (-1);
 
