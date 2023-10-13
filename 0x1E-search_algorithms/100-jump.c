8a7f298141f5fff152b7b73de6479c00bc1500af
Jealous Matsikachando <pasej5@gmail.com>
2023-10-12 18:43:59 -0700

    if jump is < 1

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -4,7 +4,7 @@
 #include <math.h>
 
 /**
- * jump_search - Searches for a value in an array using jump search algorithm.
+ * jump_search - Searches for a value in an array.
  * @array: Pointer to the first element of the array.
  * @size: The number of elements in the array.
  * @value: The value to search for.
@@ -18,7 +18,10 @@ int jump_search(int *array, size_t size, int value)
 	size_t right = 0;
 	size_t jump = sqrt(size);
 
-	jump = jump > 0 ? jump : 1;  /*Ensure jump is at least 1*/
+	if (jump <= 0)
+	{
+		jump = 1;
+	}
 	if (array == NULL || size == 0)
 		return (-1);
 
